c41f65c601bb3adc18037820be70af9cfbd3c9ed
wczhan <921391554@qq.com>
2020-11-23 14:36:19 +0800

    render process

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,16 @@
 import { hot, setConfig } from 'react-hot-loader';
 import React, { FC, useEffect } from 'react';
 import { Provider } from 'react-redux';
-import { Layout, Button } from 'antd';
+import {  Button } from 'antd';
 import { remote } from 'electron';
 
 import './styles/index.css';
 import 'antd/dist/antd.css';
 
-import styles from './index.module.scss';
+// import styles from './index.module.scss';
 import store from './store/index';
 
 const { BrowserView, BrowserWindow } = remote;
-
-const {
-  Sider,
-  Content
-} = Layout;
 
 const App: FC = () => {
   useEffect(() => {
@@ -23,18 +18,11 @@ const App: FC = () => {
   }, []);
 
   return <Provider store={store} >
-    <Layout>
-      <Sider>
-        Sider
-      </Sider>
-
-      <Content className={styles['app-content']} >
-        Content
-        <Button type="primary" >
-          Button
-        </Button>
-      </Content>
-    </Layout>
+    <div>
+      <Button>
+        test
+      </Button>
+    </div>
   </Provider> ;
 };
 
